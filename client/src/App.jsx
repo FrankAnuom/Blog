0df3bc9d35d24c900d6f3662a450812974be2d6d
Anuom Chika Frank <anuomochika@gmail.com>
2024-01-27 13:03:54 +0100

    Created a header section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,9 @@ import About from "./Pages/About";
 import SignIn from "./Pages/SignIn";
 import SignUp from "./Pages/SignUp";
 import Dashboard from "./Pages/Dashboard";
+import Header from "./Components/Header";
+import NewsLetter from "./Pages/NewsLetter";
+import Articles from "./Pages/Articles";
 
 
 
@@ -12,12 +15,15 @@ import Dashboard from "./Pages/Dashboard";
 export default function App() {
   return (
     <BrowserRouter>
+    <Header/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element ={<About />} />
         <Route path="/signin" element ={<SignIn />} />
         <Route path="/signup" element ={<SignUp/>} />
         <Route path="/dashboard" element ={<Dashboard />} />
+        <Route path="/newsletter" element ={<NewsLetter/>} />
+        <Route path="/articles" element ={<Articles/>} />
       </Routes>
     </BrowserRouter>
   );
