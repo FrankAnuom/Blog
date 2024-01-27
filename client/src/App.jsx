7d77c02cfbd2b0aebb5e1001b58478386bbc38dc
Anuom Chika Frank <anuomochika@gmail.com>
2024-01-27 09:12:06 +0100

    pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import React from "react";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import SignIn from "./Pages/SignIn";
+import SignUp from "./Pages/SignUp";
+import Dashboard from "./Pages/Dashboard";
+
+
+
 
 export default function App() {
-  return <div className="text-3xl text-red-200">App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element ={<About />} />
+        <Route path="/signin" element ={<SignIn />} />
+        <Route path="/signup" element ={<SignUp/>} />
+        <Route path="/dashboard" element ={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
